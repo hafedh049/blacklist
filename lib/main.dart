edd6e2ab2c032881f097cff9ca945f49765cfc81
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-02-12 09:19:35 +0100

    Modified  .gitignore A  .idea/libraries/Dart_SDK.xml A  .idea/libraries/KotlinJavaRuntime.xml A  .idea/modules.xml A  .idea/runConfigurations/main_dart.xml A  .idea/workspace.xml A  .metadata A  analysis_options.yaml A  blacklist.iml A  lib/main.dart A  pubspec.yaml A  test/widget_test.dart

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class Main extends StatelessWidget {
+  const Main({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
