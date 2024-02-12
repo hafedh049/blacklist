f9fc9b94c2ba188b1a76b3ece4df5bb2cf3989da
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-02-12 11:01:07 +0100

    AddedM lib/views/admin/dashboard.dart

---
+++ lib/views/admin/dashboard.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatefulWidget {
+  const Dashboard({super.key});
+
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  final List<Map<String, dynamic>> _gains = <Map<String, dynamic>>[
+    <String, dynamic>{},
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          children: <Widget>[],
+        ),
+      ),
+    );
+  }
+}
