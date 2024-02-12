91562a5035753c306ee76696ab012de98d590752
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-02-12 18:23:25 +0100

    ModifiedM lib/views/admin/products.dart

--- lib/views/admin/products.dart
+++ lib/views/admin/products.dart
@@ -9,20 +9,54 @@ class ProductsTable extends StatefulWidget {
 }
 
 class _ProductsTableState extends State<ProductsTable> {
-  final Map<String,List<dynamic>> _data =<String,List<dynamic>>{
-    'check':
-  };
+  bool _selectAll = false;
+  final Map<String, List<dynamic>> _data = <String, List<dynamic>>{};
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         padding: const EdgeInsets.all(24),
         color: darkColor,
-        child:ListView.separated(itemBuilder: (BuildContext context, int index) {
-          return SingleChildScrollView(child: Row(children: <Widget>[],),);
-        }, separatorBuilder: (BuildContext context,int index) {
-          return Container(width: MediaQuery.sizeOf(context).width,height: .5,color: greyColor,margin: const EdgeInsets.symmetric(vertical: 20),);
-        }, itemCount: 10,),
+        child: StatefulBuilder(
+          builder: (BuildContext context, void Function(void Function()) _) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SingleChildScrollView(
+                  child: Row(
+                    children: <Widget>[
+                      Checkbox(
+                        value: _selectAll,
+                        checkColor: purpleColor,
+                        onChanged: (bool? value) {
+                          setState(() {
+                            _selectAll = !_selectAll;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                Container(width: MediaQuery.sizeOf(context).width, height: .5, color: greyColor, margin: const EdgeInsets.symmetric(vertical: 20)),
+                Expanded(
+                  child: ListView.separated(
+                    itemBuilder: (BuildContext context, int index) {
+                      return SingleChildScrollView(
+                        child: Row(
+                          children: <Widget>[
+                            SizedBox(width: 100, child: Text("26/09/23")),
+                          ],
+                        ),
+                      );
+                    },
+                    separatorBuilder: (BuildContext context, int index) => Container(width: MediaQuery.sizeOf(context).width, height: .5, color: greyColor, margin: const EdgeInsets.symmetric(vertical: 20)),
+                    itemCount: 10,
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
