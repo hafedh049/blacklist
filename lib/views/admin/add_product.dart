0536ac86c048855e6d290b3600d34c59ea534898
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-02-12 22:21:43 +0100

    ModifiedM lib/views/admin/add_product.dart

--- lib/views/admin/add_product.dart
+++ lib/views/admin/add_product.dart
@@ -8,8 +8,38 @@ class AddProduct extends StatefulWidget {
 }
 
 class _AddProductState extends State<AddProduct> {
+  final TextEditingController _productNameController = TextEditingController();
+  final TextEditingController _productDateController = TextEditingController();
+  final TextEditingController _productReferenceController = TextEditingController();
+  final TextEditingController _productCategoryController = TextEditingController();
+  final TextEditingController _productOldPriceController = TextEditingController();
+  final TextEditingController _productNewPriceController = TextEditingController();
+  final TextEditingController _productQuantityController = TextEditingController();
+  final TextEditingController _productStockAlertController = TextEditingController();
+
+  @override
+  void dispose() {
+    _productNameController.dispose();
+    _productDateController.dispose();
+    _productReferenceController.dispose();
+    _productCategoryController.dispose();
+    _productOldPriceController.dispose();
+    _productNewPriceController.dispose();
+    _productQuantityController.dispose();
+    _productStockAlertController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[],
+        ),
+      ),
+    );
   }
 }
