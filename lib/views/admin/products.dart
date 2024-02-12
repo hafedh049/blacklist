00f207c6998b5d75301281e91be3e7680ef7bbd2
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-02-12 19:08:40 +0100

    ModifiedM lib/views/admin/products.dart

--- lib/views/admin/products.dart
+++ lib/views/admin/products.dart
@@ -1,5 +1,7 @@
 import 'package:blacklist/utils/shared.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:icons_plus/icons_plus.dart';
 
 class ProductsTable extends StatefulWidget {
   const ProductsTable({super.key});
@@ -10,7 +12,7 @@ class ProductsTable extends StatefulWidget {
 
 class _ProductsTableState extends State<ProductsTable> {
   bool _selectAll = false;
-  final List<String> _columns = const <String>["DATE", "REFERENCE", "PRODUCT NAME", "CATEGORY", "REAL PRICE", "NEW PRICE", "QUANTITY"];
+  final List<String> _columns = const <String>["DATE", "REFERENCE", "PRODUCT NAME", "CATEGORY", "REAL PRICE", "NEW PRICE", "QUANTITY", "STOCK ALERT"];
   final Map<String, List<dynamic>> _data = <String, List<dynamic>>{};
   @override
   Widget build(BuildContext context) {
@@ -24,6 +26,7 @@ class _ProductsTableState extends State<ProductsTable> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
                   child: Row(
                     children: <Widget>[
                       Checkbox(
@@ -35,7 +38,13 @@ class _ProductsTableState extends State<ProductsTable> {
                           });
                         },
                       ),
-                      for(final String column in _columns)...<Widget>[  Text(column),,const SizedBox(width: 10),,const SizedBox(width: 30),],
+                      const SizedBox(width: 30),
+                      for (final String column in _columns) ...<Widget>[
+                        Text(column, style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: greyColor)),
+                        const SizedBox(width: 10),
+                        const Icon(FontAwesome.sort_solid, size: 15, color: greyColor),
+                        const SizedBox(width: 60),
+                      ],
                     ],
                   ),
                 ),
@@ -44,6 +53,7 @@ class _ProductsTableState extends State<ProductsTable> {
                   child: ListView.separated(
                     itemBuilder: (BuildContext context, int index) {
                       return SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
                         child: Row(
                           children: <Widget>[
                             Checkbox(
@@ -55,7 +65,7 @@ class _ProductsTableState extends State<ProductsTable> {
                                 });
                               },
                             ),
-                            const SizedBox(width: 30),
+                            const SizedBox(width: 60),
                             Text("26/09/23"),
                           ],
                         ),
