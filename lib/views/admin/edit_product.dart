94b07f90146dec2bed2cd2ec4fd3e5988d01b1f2
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-02-13 10:34:27 +0100

    Added  lib/models/product_model.dart  M lib/views/admin/add_product.dart ?? lib/views/admin/edit_product.dart ?? lib/views/auth/

--- lib/views/admin/edit_product.dart
+++ lib/views/admin/edit_product.dart
@@ -10,14 +10,14 @@ import 'package:icons_plus/icons_plus.dart';
 
 import '../../utils/shared.dart';
 
-class AddProduct extends StatefulWidget {
-  const AddProduct({super.key});
+class EditProduct extends StatefulWidget {
+  const EditProduct({super.key});
 
   @override
-  State<AddProduct> createState() => _AddProductState();
+  State<EditProduct> createState() => _EditProductState();
 }
 
-class _AddProductState extends State<AddProduct> {
+class _EditProductState extends State<EditProduct> {
   final TextEditingController _productNameController = TextEditingController();
   final TextEditingController _productDateController = TextEditingController(text: formatDate(DateTime.now(), const <String>[yy, '-', M, '-', d, " ", HH, ':', nn, ':', ss]).toUpperCase());
   final TextEditingController _productReferenceController = TextEditingController(text: "#${List<String>.generate(8, (int index) => Random().nextInt(10).toString()).join()}");
@@ -109,7 +109,7 @@ class _AddProductState extends State<AddProduct> {
                     text: TextSpan(
                       children: <TextSpan>[
                         TextSpan(text: "Dashboard", style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: purpleColor)),
-                        TextSpan(text: " / Add Product", style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: greyColor)),
+                        TextSpan(text: " / Edit Product", style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: greyColor)),
                       ],
                     ),
                   ),
@@ -177,7 +177,7 @@ class _AddProductState extends State<AddProduct> {
               AnimatedButton(
                 width: 150,
                 height: 40,
-                text: 'SUBMIT',
+                text: 'UPDATE',
                 selectedTextColor: darkColor,
                 animatedOn: AnimatedOn.onHover,
                 animationDuration: 500.ms,
---
+++ lib/views/auth/sign_in.dart
@@ -0,0 +1,18 @@
+import 'package:blacklist/utils/shared.dart';
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  const SignIn({super.key});
+
+  @override
+  State<SignIn> createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: scaffoldColor,
+    );
+  }
+}
