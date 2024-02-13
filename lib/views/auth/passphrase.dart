f5b5d3af07cc8e5ac88d011775ae46d77f707640
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-02-14 00:25:59 +0100

    ModifiedM lib/views/auth/passphrase.dart

--- lib/views/auth/passphrase.dart
+++ lib/views/auth/passphrase.dart
@@ -26,18 +26,19 @@ class _PassphraseState extends State<Passphrase> {
   bool _buttonState = false;
   bool _passphraseState = false;
 
-  final GlobalKey<State> _buttonKey = GlobalKey<State>();
+  final GlobalKey<State> _button1Key = GlobalKey<State>();
+  final GlobalKey<State> _button2Key = GlobalKey<State>();
 
   final TextEditingController _passphrase = TextEditingController();
   final FocusNode _passphraseFocus = FocusNode();
 
-  Future<void> _signIn() async {
+  Future<void> _signIn(GlobalKey<State> key) async {
     if (_passphrase.text.trim().isEmpty) {
       showToast("Please enter the passphrase", redColor);
     } else {
-      _buttonKey.currentState!.setState(() => _buttonState = true);
+      key.currentState!.setState(() => _buttonState = true);
       await Future.delayed(200.ms);
-      _buttonKey.currentState!.setState(() => _buttonState = false);
+      key.currentState!.setState(() => _buttonState = false);
       if (sha512.convert(utf8.encode(_passphrase.text)) == sha512.convert(utf8.encode(_adminPassphrase))) {
         showToast("Welcome ADMIN", greenColor);
         // ignore: use_build_context_synchronously
@@ -65,90 +66,181 @@ class _PassphraseState extends State<Passphrase> {
     return Scaffold(
       backgroundColor: scaffoldColor,
       body: Center(
-        child: AnimatedLoadingBorder(
-          borderWidth: 4,
-          borderColor: purpleColor,
-          child: Container(
-            color: darkColor,
-            width: MediaQuery.sizeOf(context).width * .7,
-            padding: const EdgeInsets.all(24),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Text("WELCOME", style: GoogleFonts.itim(fontSize: 22, fontWeight: FontWeight.w500, color: greyColor)),
-                Container(width: MediaQuery.sizeOf(context).width, height: .3, color: greyColor, margin: const EdgeInsets.symmetric(vertical: 20)),
-                Row(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            AnimatedLoadingBorder(
+              borderWidth: 4,
+              borderColor: purpleColor,
+              child: Container(
+                color: darkColor,
+                width: MediaQuery.sizeOf(context).width * .7,
+                padding: const EdgeInsets.all(24),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   mainAxisSize: MainAxisSize.min,
                   children: <Widget>[
-                    Flexible(child: Text("Enter passphrase to continue", style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: whiteColor))),
-                    const SizedBox(width: 5),
-                    Text("*", style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: redColor)),
-                  ],
-                ),
-                const SizedBox(height: 20),
-                Container(
-                  decoration: BoxDecoration(color: scaffoldColor, borderRadius: BorderRadius.circular(3)),
-                  child: StatefulBuilder(
-                    builder: (BuildContext context, void Function(void Function()) _) {
-                      return TextField(
-                        obscureText: !_passphraseState,
-                        focusNode: _passphraseFocus,
-                        onSubmitted: (value) => _signIn(),
-                        onChanged: (String value) {
-                          if (value.trim().length <= 1) {
-                            _(() {});
-                          }
-                        },
-                        controller: _passphrase,
-                        style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: greyColor),
-                        decoration: InputDecoration(
-                          contentPadding: const EdgeInsets.all(20),
-                          focusedBorder: const OutlineInputBorder(borderSide: BorderSide(color: purpleColor, width: 2, style: BorderStyle.solid)),
-                          border: InputBorder.none,
-                          hintText: 'Passphrase',
-                          hintStyle: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: greyColor),
-                          prefixIcon: _passphrase.text.trim().isEmpty ? null : const Icon(FontAwesome.circle_check_solid, size: 15, color: greenColor),
-                          suffixIcon: IconButton(onPressed: () => _(() => _passphraseState = !_passphraseState), icon: Icon(_passphraseState ? FontAwesome.eye_solid : FontAwesome.eye_slash_solid, size: 15, color: purpleColor)),
-                        ),
-                        cursorColor: purpleColor,
-                      );
-                    },
-                  ),
-                ),
-                const SizedBox(height: 20),
-                StatefulBuilder(
-                  key: _buttonKey,
-                  builder: (BuildContext context, void Function(void Function()) _) {
-                    return Row(
+                    Text("ADMIN", style: GoogleFonts.itim(fontSize: 22, fontWeight: FontWeight.w500, color: greyColor)),
+                    Container(width: MediaQuery.sizeOf(context).width, height: .3, color: greyColor, margin: const EdgeInsets.symmetric(vertical: 20)),
+                    Row(
                       mainAxisSize: MainAxisSize.min,
                       children: <Widget>[
-                        IgnorePointer(
-                          ignoring: _buttonState,
-                          child: AnimatedButton(
-                            width: 150,
-                            height: 40,
-                            text: _buttonState ? "WAIT..." : 'CONTINUE',
-                            selectedTextColor: darkColor,
-                            animatedOn: AnimatedOn.onHover,
-                            animationDuration: 500.ms,
-                            isReverse: true,
-                            selectedBackgroundColor: redColor,
-                            backgroundColor: purpleColor,
-                            transitionType: TransitionType.TOP_TO_BOTTOM,
-                            textStyle: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: whiteColor),
-                            onPress: _signIn,
-                          ),
-                        ),
-                        const SizedBox(width: 10),
-                        AnimatedOpacity(opacity: _buttonState ? 1 : 0, duration: 300.ms, child: const Icon(FontAwesome.bookmark_solid, color: purpleColor, size: 35)),
+                        Flexible(child: Text("Enter passphrase to continue", style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: whiteColor))),
+                        const SizedBox(width: 5),
+                        Text("*", style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: redColor)),
                       ],
-                    );
-                  },
+                    ),
+                    const SizedBox(height: 20),
+                    Container(
+                      decoration: BoxDecoration(color: scaffoldColor, borderRadius: BorderRadius.circular(3)),
+                      child: StatefulBuilder(
+                        builder: (BuildContext context, void Function(void Function()) _) {
+                          return TextField(
+                            obscureText: !_passphraseState,
+                            focusNode: _passphraseFocus,
+                            onSubmitted: (value) => _signIn(_button1Key),
+                            onChanged: (String value) {
+                              if (value.trim().length <= 1) {
+                                _(() {});
+                              }
+                            },
+                            controller: _passphrase,
+                            style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: greyColor),
+                            decoration: InputDecoration(
+                              contentPadding: const EdgeInsets.all(20),
+                              focusedBorder: const OutlineInputBorder(borderSide: BorderSide(color: purpleColor, width: 2, style: BorderStyle.solid)),
+                              border: InputBorder.none,
+                              hintText: 'Passphrase',
+                              hintStyle: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: greyColor),
+                              prefixIcon: _passphrase.text.trim().isEmpty ? null : const Icon(FontAwesome.circle_check_solid, size: 15, color: greenColor),
+                              suffixIcon: IconButton(onPressed: () => _(() => _passphraseState = !_passphraseState), icon: Icon(_passphraseState ? FontAwesome.eye_solid : FontAwesome.eye_slash_solid, size: 15, color: purpleColor)),
+                            ),
+                            cursorColor: purpleColor,
+                          );
+                        },
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    StatefulBuilder(
+                      key: _button1Key,
+                      builder: (BuildContext context, void Function(void Function()) _) {
+                        return Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            IgnorePointer(
+                              ignoring: _buttonState,
+                              child: AnimatedButton(
+                                width: 150,
+                                height: 40,
+                                text: _buttonState ? "WAIT..." : 'CONTINUE',
+                                selectedTextColor: darkColor,
+                                animatedOn: AnimatedOn.onHover,
+                                animationDuration: 500.ms,
+                                isReverse: true,
+                                selectedBackgroundColor: redColor,
+                                backgroundColor: purpleColor,
+                                transitionType: TransitionType.TOP_TO_BOTTOM,
+                                textStyle: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: whiteColor),
+                                onPress: () => _signIn(_button1Key),
+                              ),
+                            ),
+                            const SizedBox(width: 10),
+                            AnimatedOpacity(opacity: _buttonState ? 1 : 0, duration: 300.ms, child: const Icon(FontAwesome.bookmark_solid, color: purpleColor, size: 35)),
+                          ],
+                        );
+                      },
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
-          ),
+            const SizedBox(height: 20),
+            AnimatedLoadingBorder(
+              borderWidth: 4,
+              borderColor: purpleColor,
+              child: Container(
+                color: darkColor,
+                width: MediaQuery.sizeOf(context).width * .7,
+                padding: const EdgeInsets.all(24),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Text("VENDOR", style: GoogleFonts.itim(fontSize: 22, fontWeight: FontWeight.w500, color: greyColor)),
+                    Container(width: MediaQuery.sizeOf(context).width, height: .3, color: greyColor, margin: const EdgeInsets.symmetric(vertical: 20)),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Flexible(child: Text("Enter passphrase to continue", style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: whiteColor))),
+                        const SizedBox(width: 5),
+                        Text("*", style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: redColor)),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    Container(
+                      decoration: BoxDecoration(color: scaffoldColor, borderRadius: BorderRadius.circular(3)),
+                      child: StatefulBuilder(
+                        builder: (BuildContext context, void Function(void Function()) _) {
+                          return TextField(
+                            obscureText: !_passphraseState,
+                            focusNode: _passphraseFocus,
+                            onSubmitted: (value) => _signIn(_button2Key),
+                            onChanged: (String value) {
+                              if (value.trim().length <= 1) {
+                                _(() {});
+                              }
+                            },
+                            controller: _passphrase,
+                            style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: greyColor),
+                            decoration: InputDecoration(
+                              contentPadding: const EdgeInsets.all(20),
+                              focusedBorder: const OutlineInputBorder(borderSide: BorderSide(color: purpleColor, width: 2, style: BorderStyle.solid)),
+                              border: InputBorder.none,
+                              hintText: 'Passphrase',
+                              hintStyle: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: greyColor),
+                              prefixIcon: _passphrase.text.trim().isEmpty ? null : const Icon(FontAwesome.circle_check_solid, size: 15, color: greenColor),
+                              suffixIcon: IconButton(onPressed: () => _(() => _passphraseState = !_passphraseState), icon: Icon(_passphraseState ? FontAwesome.eye_solid : FontAwesome.eye_slash_solid, size: 15, color: purpleColor)),
+                            ),
+                            cursorColor: purpleColor,
+                          );
+                        },
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    StatefulBuilder(
+                      key: _button2Key,
+                      builder: (BuildContext context, void Function(void Function()) _) {
+                        return Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            IgnorePointer(
+                              ignoring: _buttonState,
+                              child: AnimatedButton(
+                                width: 150,
+                                height: 40,
+                                text: _buttonState ? "WAIT..." : 'CONTINUE',
+                                selectedTextColor: darkColor,
+                                animatedOn: AnimatedOn.onHover,
+                                animationDuration: 500.ms,
+                                isReverse: true,
+                                selectedBackgroundColor: redColor,
+                                backgroundColor: purpleColor,
+                                transitionType: TransitionType.TOP_TO_BOTTOM,
+                                textStyle: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: whiteColor),
+                                onPress: () => _signIn(_button2Key),
+                              ),
+                            ),
+                            const SizedBox(width: 10),
+                            AnimatedOpacity(opacity: _buttonState ? 1 : 0, duration: 300.ms, child: const Icon(FontAwesome.bookmark_solid, color: purpleColor, size: 35)),
+                          ],
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
