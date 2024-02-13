7ee9711d63124f4e10621f9249e8bbd6f7111c87
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-02-13 14:43:08 +0100

    ModifiedM lib/views/admin/stores.dart

--- lib/views/admin/stores.dart
+++ lib/views/admin/stores.dart
@@ -12,58 +12,71 @@ class StoresList extends StatefulWidget {
 }
 
 class _StoresListState extends State<StoresList> {
-  final List<Map<String, dynamic>> _stores = List<Map<String, dynamic>>.generate(20, (int index) => <String, dynamic>{"store_name": "Store ${îndex+1}", "verndor_name": "Vendor ${îndex+1}", "total_products": Random().nextInt(4000)});
+  final List<Map<String, dynamic>> _stores = List<Map<String, dynamic>>.generate(20, (int index) => <String, dynamic>{"store_name": "Store ${index + 1}", "vendor_name": "Vendor ${index + 1}", "total_products": Random().nextInt(4000).toString()});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: scaffoldColor,
       body: Padding(
         padding: const EdgeInsets.all(24),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Text("Stores", style: GoogleFonts.itim(fontSize: 22, fontWeight: FontWeight.w500, color: greyColor)),
-                const Spacer(),
-                RichText(
-                  text: TextSpan(
-                    children: <TextSpan>[
-                      TextSpan(text: "Stores", style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: purpleColor)),
-                      TextSpan(text: " / Add(Choose) store", style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: greyColor)),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            Container(width: MediaQuery.sizeOf(context).width, height: .3, color: greyColor, margin: const EdgeInsets.symmetric(vertical: 20)),
-            Center(
-              child: Wrap(
-                alignment: WrapAlignment.center,
-                crossAxisAlignment: WrapCrossAlignment.center,
-                runAlignment: WrapAlignment.center,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
                 children: <Widget>[
-                  for (final Map<String, dynamic> item in _stores)
-                    Container(
-                      width: 200,
-                      height: 200,
-                      padding: const EdgeInsets.all(16),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Text(item["store"], style: GoogleFonts.itim(fontSize: 22, fontWeight: FontWeight.w500, color: greyColor)),
-                          const SizedBox(height: 10),
-                          Text("Store Vendor", style: GoogleFonts.itim(fontSize: 20, fontWeight: FontWeight.w500, color: redColor)),
-                          const SizedBox(height: 10),
-                          Text("Store Vendor", style: GoogleFonts.itim(fontSize: 18, fontWeight: FontWeight.w500, color: greenColor)),
-                        ],
-                      ),
+                  Text("Stores", style: GoogleFonts.itim(fontSize: 22, fontWeight: FontWeight.w500, color: greyColor)),
+                  const Spacer(),
+                  RichText(
+                    text: TextSpan(
+                      children: <TextSpan>[
+                        TextSpan(text: "Stores", style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: purpleColor)),
+                        TextSpan(text: " / Add(Choose) store", style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: greyColor)),
+                      ],
                     ),
+                  ),
                 ],
               ),
-            ),
-          ],
+              Container(width: MediaQuery.sizeOf(context).width, height: .3, color: greyColor, margin: const EdgeInsets.symmetric(vertical: 20)),
+              Center(
+                child: Wrap(
+                  alignment: WrapAlignment.center,
+                  crossAxisAlignment: WrapCrossAlignment.center,
+                  runAlignment: WrapAlignment.center,
+                  runSpacing: 20,
+                  spacing: 20,
+                  children: <Widget>[
+                    for (final Map<String, dynamic> item in _stores)
+                      Container(
+                        width: 250,
+                        padding: const EdgeInsets.all(16),
+                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(5), color: darkColor),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Row(
+                              children: <Widget>[],
+                            ),
+                            Text("Store", style: GoogleFonts.itim(fontSize: 22, fontWeight: FontWeight.w500, color: greyColor)),
+                            const SizedBox(width: 10),
+                            Text(item["store_name"], style: GoogleFonts.itim(fontSize: 22, fontWeight: FontWeight.w500, color: greyColor)),
+                            const SizedBox(height: 10),
+                            Text("Vendor", style: GoogleFonts.itim(fontSize: 22, fontWeight: FontWeight.w500, color: greyColor)),
+                            const SizedBox(height: 10),
+                            Text(item["vendor_name"], style: GoogleFonts.itim(fontSize: 20, fontWeight: FontWeight.w500, color: redColor)),
+                            const SizedBox(height: 10),
+                            Text("Total Products", style: GoogleFonts.itim(fontSize: 22, fontWeight: FontWeight.w500, color: greyColor)),
+                            const SizedBox(height: 10),
+                            Text(item["total_products"], style: GoogleFonts.itim(fontSize: 18, fontWeight: FontWeight.w500, color: greenColor)),
+                          ],
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
