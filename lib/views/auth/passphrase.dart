e5401d8df811591f2e6bf6a56027f272f8d40272
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-02-13 12:08:30 +0100

    Update passphrase.dart

--- lib/views/auth/passphrase.dart
+++ lib/views/auth/passphrase.dart
@@ -21,10 +21,34 @@ class _PassphraseState extends State<Passphrase> {
   final String _adminPassphrase = "admin";
   final String _vendorPassphrase = "vendor";
 
+  bool _buttonState = false;
+
+  final GlobalKey<State> _buttonKey = GlobalKey<State>();
+
   final TextEditingController _passphrase = TextEditingController();
+  final FocusNode _passphraseFocus = FocusNode();
+
+  Future<void> _signIn() async {
+    if (_passphrase.text.trim().isEmpty) {
+      showToast("Please enter the passphrase", redColor);
+    } else {
+      _buttonKey.currentState!.setState(() => _buttonState = true);
+      if (sha512.convert(utf8.encode(_passphrase.text)) == sha512.convert(utf8.encode(_adminPassphrase))) {
+        showToast("Welcome ADMIN", greenColor);
+      } else if (sha512.convert(utf8.encode(_passphrase.text)) == sha512.convert(utf8.encode(_vendorPassphrase))) {
+        showToast("Welcome VENDOR", greenColor);
+      } else {
+        showToast("Wrong Credentials", redColor);
+        _passphraseFocus.requestFocus();
+        _buttonKey.currentState!.setState(() => _buttonState = false);
+      }
+    }
+  }
+
   @override
   void dispose() {
     _passphrase.dispose();
+    _passphraseFocus.dispose();
     super.dispose();
   }
 
@@ -60,6 +84,8 @@ class _PassphraseState extends State<Passphrase> {
                   child: StatefulBuilder(
                     builder: (BuildContext context, void Function(void Function()) _) {
                       return TextField(
+                        focusNode: _passphraseFocus,
+                        onSubmitted: (value) => _signIn(),
                         onChanged: (String value) {
                           if (value.trim().length <= 1) {
                             _(() {});
@@ -81,30 +107,26 @@ class _PassphraseState extends State<Passphrase> {
                   ),
                 ),
                 const SizedBox(height: 20),
-                AnimatedButton(
-                  width: 150,
-                  height: 40,
-                  text: 'CONTINUE',
-                  selectedTextColor: darkColor,
-                  animatedOn: AnimatedOn.onHover,
-                  animationDuration: 500.ms,
-                  isReverse: true,
-                  selectedBackgroundColor: redColor,
-                  backgroundColor: purpleColor,
-                  transitionType: TransitionType.TOP_TO_BOTTOM,
-                  textStyle: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: whiteColor),
-                  onPress: () {
-                    if (_passphrase.text.trim().isEmpty) {
-                      showToast("Please enter the passphrase", redColor);
-                    } else {
-                      if (sha512.convert(utf8.encode(_passphrase.text)) == sha512.convert(utf8.encode(_adminPassphrase))) {
-                        showToast("Welcome ADMIN", greenColor);
-                      } else if (sha512.convert(utf8.encode(_passphrase.text)) == sha512.convert(utf8.encode(_vendorPassphrase))) {
-                        showToast("Welcome VENDOR", greenColor);
-                      } else {
-                        showToast("Wrong Credentials", redColor);
-                      }
-                    }
+                StatefulBuilder(
+                  key: _buttonKey,
+                  builder: (BuildContext context, void Function(void Function()) _) {
+                    return IgnorePointer(
+                      ignoring: _buttonState,
+                      child: AnimatedButton(
+                        width: 150,
+                        height: 40,
+                        text: _buttonState ? "WAIT..." : 'CONTINUE',
+                        selectedTextColor: darkColor,
+                        animatedOn: AnimatedOn.onHover,
+                        animationDuration: 500.ms,
+                        isReverse: true,
+                        selectedBackgroundColor: redColor,
+                        backgroundColor: purpleColor,
+                        transitionType: TransitionType.TOP_TO_BOTTOM,
+                        textStyle: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: whiteColor),
+                        onPress: _signIn,
+                      ),
+                    );
                   },
                 ),
               ],
