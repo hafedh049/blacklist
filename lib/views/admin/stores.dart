7afe8137c6f80436887b80cebde128040f8e7100
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-02-13 15:43:07 +0100

    Update stores.dart

--- lib/views/admin/stores.dart
+++ lib/views/admin/stores.dart
@@ -1,7 +1,7 @@
 import 'dart:math';
 
 import 'package:blacklist/utils/shared.dart';
-import 'package:blacklist/views/admin/dashboard.dart';
+import 'package:blacklist/views/admin/dashboard/dashboard.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
