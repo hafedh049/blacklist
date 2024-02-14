a987f91f15b70d9dda0d287285402158b9229f5e
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-02-14 21:34:23 +0100

    ModifiedM lib/views/admin/products_table.dart

--- lib/views/admin/products_table.dart
+++ lib/views/admin/products_table.dart
@@ -17,10 +17,18 @@ class _ProductsTableState extends State<ProductsTable> {
   final List<String> _columns = const <String>["DATE", "REFERENCE", "PRODUCT NAME", "CATEGORY", "REAL PRICE", "NEW PRICE", "QUANTITY", "STOCK ALERT"];
   late final List<Map<String, dynamic>> _data;
 
+  final PageController _pageController = PageController();
+
   @override
   void initState() {
     _data = List<Map<String, dynamic>>.generate(1000, (int index) => <String, dynamic>{for (final String key in _columns) key: key});
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
   }
 
   @override
@@ -60,30 +68,27 @@ class _ProductsTableState extends State<ProductsTable> {
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        SingleChildScrollView(
-                          scrollDirection: Axis.horizontal,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              Checkbox(
-                                value: _selectAll,
-                                checkColor: purpleColor,
-                                onChanged: (bool? value) {
-                                  setState(() {
-                                    _selectAll = !_selectAll;
-                                  });
-                                },
-                              ),
-                              const SizedBox(width: 30),
-                              for (final String column in _columns) ...<Widget>[
-                                Text(column, style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: purpleColor)),
-                                const SizedBox(width: 10),
-                                const Icon(FontAwesome.sort_solid, size: 15, color: purpleColor),
-                                const SizedBox(width: 60),
-                              ],
-                              Text("ACTION", style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: purpleColor)),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Checkbox(
+                              value: _selectAll,
+                              checkColor: purpleColor,
+                              onChanged: (bool? value) {
+                                setState(() {
+                                  _selectAll = !_selectAll;
+                                });
+                              },
+                            ),
+                            const SizedBox(width: 30),
+                            for (final String column in _columns) ...<Widget>[
+                              Text(column, style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: purpleColor)),
+                              const SizedBox(width: 10),
+                              const Icon(FontAwesome.sort_solid, size: 15, color: purpleColor),
+                              const SizedBox(width: 60),
                             ],
-                          ),
+                            Text("ACTION", style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: purpleColor)),
+                          ],
                         ),
                         Container(width: MediaQuery.sizeOf(context).width, height: .5, color: greyColor, margin: const EdgeInsets.symmetric(vertical: 20)),
                         Expanded(
@@ -96,37 +101,48 @@ class _ProductsTableState extends State<ProductsTable> {
                                 itemCount: data.length,
                                 itemBuilder: (BuildContext context, int index) {
                                   return SingleChildScrollView(
-                                    scrollDirection: Axis.horizontal,
-                                    child: Row(
-                                      mainAxisSize: MainAxisSize.min,
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
                                       children: <Widget>[
-                                        Checkbox(
-                                          value: _selectAll,
-                                          checkColor: purpleColor,
-                                          onChanged: (bool? value) => setState(() => _selectAll = !_selectAll),
-                                        ),
-                                        const SizedBox(width: 30),
-                                        for (final String column in _columns) ...<Widget>[
-                                          Text(column, style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: greyColor)),
-                                          const SizedBox(width: 85),
-                                        ],
-                                        Row(
-                                          mainAxisSize: MainAxisSize.min,
-                                          children: <Widget>[
-                                            IconButton(
-                                              onPressed: () {},
-                                              icon: const Icon(FontAwesome.eye_solid, color: greenColor, size: 15),
-                                            ),
-                                            IconButton(
-                                              onPressed: () {},
-                                              icon: const Icon(FontAwesome.pen_solid, color: purpleColor, size: 15),
-                                            ),
-                                            IconButton(
-                                              onPressed: () {},
-                                              icon: const Icon(FontAwesome.circle_xmark_solid, color: redColor, size: 15),
-                                            ),
+                                        for (Map<String, dynamic> map in data[index]) ...<Widget>[
+                                          Row(
+                                            mainAxisSize: MainAxisSize.min,
+                                            children: <Widget>[
+                                              Checkbox(
+                                                value: _selectAll,
+                                                checkColor: purpleColor,
+                                                onChanged: (bool? value) => setState(() => _selectAll = !_selectAll),
+                                              ),
+                                              const SizedBox(width: 30),
+                                              for (final String key in map.keys) ...<Widget>[
+                                                Text(key, style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: greyColor)),
+                                                const SizedBox(width: 85),
+                                              ],
+                                              Row(
+                                                mainAxisSize: MainAxisSize.min,
+                                                children: <Widget>[
+                                                  IconButton(
+                                                    onPressed: () {},
+                                                    icon: const Icon(FontAwesome.eye_solid, color: greenColor, size: 15),
+                                                  ),
+                                                  IconButton(
+                                                    onPressed: () {},
+                                                    icon: const Icon(FontAwesome.pen_solid, color: purpleColor, size: 15),
+                                                  ),
+                                                  IconButton(
+                                                    onPressed: () {},
+                                                    icon: const Icon(FontAwesome.circle_xmark_solid, color: redColor, size: 15),
+                                                  ),
+                                                ],
+                                              ),
+                                            ],
+                                          ),
+                                          if (map != data[index].last) ...<Widget>[
+                                            const SizedBox(height: 20),
+                                            const Divider(thickness: .3, height: .3, color: greyColor),
+                                            const SizedBox(height: 20),
                                           ],
-                                        ),
+                                        ],
                                       ],
                                     ),
                                   );
@@ -135,53 +151,21 @@ class _ProductsTableState extends State<ProductsTable> {
                             },
                           ),
                         ),
-                        /*Expanded(
-                          child: ListView.separated(
-                            itemBuilder: (BuildContext context, int index) {
-                              return SingleChildScrollView(
-                                scrollDirection: Axis.horizontal,
-                                child: Row(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: <Widget>[
-                                    Checkbox(
-                                      value: _selectAll,
-                                      checkColor: purpleColor,
-                                      onChanged: (bool? value) {
-                                        setState(() {
-                                          _selectAll = !_selectAll;
-                                        });
-                                      },
-                                    ),
-                                    const SizedBox(width: 30),
-                                    for (final String column in _columns) ...<Widget>[
-                                      Text(column, style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: greyColor)),
-                                      const SizedBox(width: 85),
-                                    ],
-                                    Row(
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: <Widget>[
-                                        IconButton(
-                                          onPressed: () {},
-                                          icon: const Icon(FontAwesome.eye_solid, color: greenColor, size: 15),
-                                        ),
-                                        IconButton(
-                                          onPressed: () {},
-                                          icon: const Icon(FontAwesome.pen_solid, color: purpleColor, size: 15),
-                                        ),
-                                        IconButton(
-                                          onPressed: () {},
-                                          icon: const Icon(FontAwesome.circle_xmark_solid, color: redColor, size: 15),
-                                        ),
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                              );
-                            },
-                            separatorBuilder: (BuildContext context, int index) => Container(width: MediaQuery.sizeOf(context).width, height: .3, color: greyColor, margin: const EdgeInsets.symmetric(vertical: 20)),
-                            itemCount: 1000,
-                          ),
-                        ),*/
+                        const SizedBox(height: 20),
+                        Row(
+                          children: <Widget>[
+                            const Spacer(),
+                            IconButton(
+                              onPressed: () {},
+                              icon: const Icon(FontAwesome.chevron_left_solid, color: purpleColor, size: 15),
+                            ),
+                            Text(key, style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: greyColor)),
+                            IconButton(
+                              onPressed: () {},
+                              icon: const Icon(FontAwesome.chevron_right_solid, color: purpleColor, size: 15),
+                            ),
+                          ],
+                        ),
                       ],
                     ),
                   ),
