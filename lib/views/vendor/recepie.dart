9db55b98087e04c0ad4693681d27408aba11811d
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-02-20 11:54:31 +0100

    ModifiedM lib/views/vendor/recepie.dart

--- lib/views/vendor/recepie.dart
+++ lib/views/vendor/recepie.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:blacklist/utils/shared.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Recepie extends StatefulWidget {
   const Recepie({super.key});
@@ -14,8 +15,7 @@ class _RecepieState extends State<Recepie> {
   final List<Map<String, dynamic>> _recepies = List<Map<String, dynamic>>.generate(
     100,
     (int index) => <String, dynamic>{
-      "username": <String>["User ${index + 1}", "Anonymous"][Random().nextInt(2)],
-      "birth_date": DateTime.fromMillisecondsSinceEpoch(Random().nextInt(4000) * Random().nextInt(4000)),
+      "client": <String>["User ${index + 1}", "Anonymous"][Random().nextInt(2)],
       "cin": List<String>.generate(8, (int index) => Random().nextInt(10).toString()).join(),
       "products": List<String>.generate(
         20,
@@ -32,10 +32,38 @@ class _RecepieState extends State<Recepie> {
           Expanded(
             child: ListView.separated(
               itemBuilder: (BuildContext context, int index) => Container(
+                padding: const EdgeInsets.all(4),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[],
+                  children: <Widget>[
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Container(
+                          padding: const EdgeInsets.all(4),
+                          decoration: BoxDecoration(color: _recepies[index]["client"] == "Anonymous" ? greenColor : redColor),
+                          child: Text("Client", style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: whiteColor)),
+                        ),
+                        const SizedBox(width: 10),
+                        Text(_recepies[index]["client"], style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: greyColor)),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Container(
+                          padding: const EdgeInsets.all(4),
+                          decoration: const BoxDecoration(color: greenColor),
+                          child: Text("CIN", style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: whiteColor)),
+                        ),
+                        const SizedBox(width: 10),
+                        Text(_recepies[index]["cin"], style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: greyColor)),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                  ],
                 ),
               ),
               separatorBuilder: (BuildContext context, int index) => const SizedBox(height: 20),
