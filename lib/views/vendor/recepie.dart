b843a3cd98aaf3400f18edbbfb380bf222e55258
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-02-20 11:34:30 +0100

    ModifiedM lib/views/vendor/recepie.dart

--- lib/views/vendor/recepie.dart
+++ lib/views/vendor/recepie.dart
@@ -16,6 +16,10 @@ class _RecepieState extends State<Recepie> {
       "username": <String>["User ${index + 1}", "Anonymous"][Random().nextInt(2)],
       "birth_date": DateTime.fromMillisecondsSinceEpoch(Random().nextInt(4000) * Random().nextInt(4000)),
       "cin": List<String>.generate(8, (int index) => Random().nextInt(10).toString()).join(),
+      "products": List<String>.generate(
+        20,
+        (int index) => "${index + 1} ο ${["Vape", "Meat", "Fish", "Steak", "Salami", "Cheese"][Random().nextInt(6)]} / Product ο (Quantity) ➤ [Price] DT",
+      ),
     },
   );
   @override
