955347587f2e3a3b6fcfeb6ad8f54b8ef44fe090
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-02-20 10:15:44 +0100

    ModifiedM lib/views/vendor/cart_recipe.dart

--- lib/views/vendor/cart_recipe.dart
+++ lib/views/vendor/cart_recipe.dart
@@ -1,5 +1,8 @@
+import 'package:animated_loading_border/animated_loading_border.dart';
+import 'package:blacklist/utils/shared.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CartRecepie extends StatefulWidget {
   const CartRecepie({super.key});
@@ -17,6 +20,12 @@ class _CartRecepieState extends State<CartRecepie> {
       <String, dynamic>{
         "title": "Cart",
         "callback": () => Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => Container())),
+        "selected": false,
+      },
+      <String, dynamic>{
+        "title": "Recepie",
+        "callback": () => Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => Container())),
+        "selected": false,
       },
     ];
     super.initState();
@@ -25,22 +34,34 @@ class _CartRecepieState extends State<CartRecepie> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: scaffoldColor,
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(24),
-          child: StatefulBuilder(builder: (BuildContext context, void Function(void Function()) _) {
-            return Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                for (final Map<String, dynamic> item in _items)
-                  AnimatedContainer(
-                    duration: 300.ms,
-                    padding: const EdgeInsets.all(24),
-                  ),
-              ],
-            );
-          }),
+          child: StatefulBuilder(
+            builder: (BuildContext context, void Function(void Function()) _) {
+              return Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  for (final Map<String, dynamic> item in _items)
+                    AnimatedLoadingBorder(
+                      borderWidth: 4,
+                      borderColor: purpleColor,
+                      child: Container(
+                        padding: const EdgeInsets.all(24),
+                        decoration: BoxDecoration(
+                          color: item["selected"] ? purpleColor : darkColor,
+                          border: Border.all(color: item["selected"] ? purpleColor : darkColor),
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        child: Text(item["title"], style: GoogleFonts.itim(fontSize: 18, fontWeight: FontWeight.w500, color: whiteColor)),
+                      ),
+                    ),
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
