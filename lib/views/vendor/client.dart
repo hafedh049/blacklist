59223bc4b5c4d01f7f87a925ce3152d4f74cbd23
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-02-21 17:21:37 +0100

    ModifiedM lib/views/vendor/client.dart

--- lib/views/vendor/client.dart
+++ lib/views/vendor/client.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 
 import 'package:animated_loading_border/animated_loading_border.dart';
 import 'package:blacklist/utils/shared.dart';
+import 'package:comment_tree/comment_tree.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -20,14 +21,16 @@ class _ClientState extends State<Client> {
   final GlobalKey<State> _qrKey = GlobalKey<State>();
   String _data = List<String>.generate(8, (int index) => Random().nextInt(10).toString()).join();
 
-  final List<Map<String, dynamic>> _products = List<Map<String, dynamic>>.generate(
-    10,
-    (int index) => <String, dynamic>{
-      "category": "Category ${index + 1}",
-      "product": "Product ${index + 1}",
-      "total_buys": Random().nextInt(4000),
-    },
-  );
+  final Map<String, List<Map<String, dynamic>>> _products = <String, List<Map<String, dynamic>>>{
+    for (int index = 0; index < 10; index += 1)
+      "Category ${index + 1}": <Map<String, dynamic>>[
+        for (int indexJ = 0; indexJ < 5; indexJ += 1)
+          <String, dynamic>{
+            "product": "Product ${indexJ + 1}",
+            "total_buys": Random().nextInt(4000),
+          },
+      ],
+  };
 
   @override
   void initState() {
@@ -62,22 +65,24 @@ class _ClientState extends State<Client> {
                     builder: (BuildContext context) => SizedBox(
                       width: MediaQuery.sizeOf(context).width * .7,
                       height: MediaQuery.sizeOf(context).height * .4,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Container(
-                            padding: const EdgeInsets.all(5),
-                            decoration: BoxDecoration(color: greenColor, borderRadius: BorderRadius.circular(5)),
-                            child: Text("Foulen Fouleni", style: GoogleFonts.itim(fontSize: 16, color: greyColor, fontWeight: FontWeight.w500)),
-                          ),
-                          const SizedBox(height: 20),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisSize: MainAxisSize.min,
-                            children: <Widget>[],
-                          ),
-                        ],
+                      child: SingleChildScrollView(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Container(
+                              padding: const EdgeInsets.all(5),
+                              decoration: BoxDecoration(color: greenColor, borderRadius: BorderRadius.circular(5)),
+                              child: Text("Foulen Fouleni", style: GoogleFonts.itim(fontSize: 16, color: greyColor, fontWeight: FontWeight.w500)),
+                            ),
+                            const SizedBox(height: 20),
+                            for (final MapEntry<String, List<Map<String, dynamic>>> tuple in _products.entries)
+                              CommentTreeWidget<String, Map<String, dynamic>>(
+                                tuple.key,
+                                tuple.value,
+                              ),
+                          ],
+                        ),
                       ),
                     ),
                   );
