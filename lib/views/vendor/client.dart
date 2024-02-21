9371b4cb43468c0ac0015c223808132ecfc8e4d0
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-02-21 18:39:23 +0100

    Modified  lib/views/admin/add_product.dart MM lib/views/vendor/client.dart

--- lib/views/vendor/client.dart
+++ lib/views/vendor/client.dart
@@ -4,8 +4,11 @@ import 'dart:math';
 import 'package:animated_loading_border/animated_loading_border.dart';
 import 'package:blacklist/utils/shared.dart';
 import 'package:comment_tree/comment_tree.dart';
+import 'package:date_format/date_format.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_animate/flutter_animate.dart';
+import 'package:flutter_animated_button/flutter_animated_button.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:icons_plus/icons_plus.dart';
 import 'package:pretty_qr_code/pretty_qr_code.dart';
@@ -32,6 +35,30 @@ class _ClientState extends State<Client> {
           },
       ],
   };
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _birthDateController = TextEditingController(text: formatDate(DateTime.now(), const <String>[yy, '-', M, '-', d, " ", HH, ':', nn, ':', ss]).toUpperCase());
+  final TextEditingController _cinController = TextEditingController(text: "#${List<String>.generate(8, (int index) => Random().nextInt(10).toString()).join()}");
+
+  late final Map<String, Map<String, dynamic>> _productTemplate = <String, Map<String, dynamic>>{
+    "Full name": <String, dynamic>{
+      "controller": _usernameController,
+      "type": "text",
+      "required": true,
+      "hint": "Choose Category",
+    },
+    "Product Name": <String, dynamic>{
+      "controller": _birthDateController,
+      "type": "text",
+      "required": true,
+      "hint": "Choose Name",
+    },
+    "Date": <String, dynamic>{
+      "controller": _cinController,
+      "type": "date",
+      "required": false,
+      "hint": "",
+    },
+  };
 
   @override
   void initState() {
@@ -41,6 +68,9 @@ class _ClientState extends State<Client> {
 
   @override
   void dispose() {
+    _usernameController.dispose();
+    _birthDateController.dispose();
+    _cinController.dispose();
     _timer.cancel();
     super.dispose();
   }
@@ -180,11 +210,78 @@ class _ClientState extends State<Client> {
                       mainAxisSize: MainAxisSize.min,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
+                        for (final MapEntry<String, Map<String, dynamic>> entry in _productTemplate.entries)
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: <Widget>[
+                                  Text(entry.key, style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: whiteColor)),
+                                  const SizedBox(width: 5),
+                                  Text("*", style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: entry.value["required"] ? redColor : greenColor)),
+                                ],
+                              ),
+                              const SizedBox(height: 20),
+                              Container(
+                                color: darkColor,
+                                child: StatefulBuilder(
+                                  builder: (BuildContext context, void Function(void Function()) _) {
+                                    return TextField(
+                                      onChanged: (String value) {
+                                        if (value.trim().length <= 1) {
+                                          _(() {});
+                                        }
+                                      },
+                                      controller: entry.value["controller"],
+                                      readOnly: entry.value["type"] == "date" || entry.value["type"] == "reference" ? true : false,
+                                      style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: greyColor),
+                                      decoration: InputDecoration(
+                                        contentPadding: const EdgeInsets.all(20),
+                                        focusedBorder: const OutlineInputBorder(borderSide: BorderSide(color: purpleColor, width: 2, style: BorderStyle.solid)),
+                                        border: InputBorder.none,
+                                        hintText: entry.value["hint"],
+                                        hintStyle: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: greyColor),
+                                        suffixIcon: entry.value["controller"].text.trim().isEmpty ? null : const Icon(FontAwesome.circle_check_solid, size: 15, color: greenColor),
+                                      ),
+                                      cursorColor: purpleColor,
+                                      inputFormatters: <TextInputFormatter>[
+                                        FilteringTextInputFormatter.allow(
+                                          RegExp(
+                                            entry.value["type"] == "double"
+                                                ? r"[\d.]"
+                                                : entry.value["type"] == "number"
+                                                    ? r"\d"
+                                                    : r".",
+                                          ),
+                                        ),
+                                      ],
+                                    );
+                                  },
+                                ),
+                              ),
+                              const SizedBox(height: 20),
+                            ],
+                          ),
                         const SizedBox(height: 20),
                         Row(
                           children: <Widget>[
                             const Spacer(),
-                            Container(child:  Text("NEXT", style: GoogleFonts.itim(fontSize: 22, fontWeight: FontWeight.w500, color: greyColor)),,),
+                            AnimatedButton(
+                              width: 150,
+                              height: 40,
+                              text: 'NEXT',
+                              selectedTextColor: darkColor,
+                              animatedOn: AnimatedOn.onHover,
+                              animationDuration: 500.ms,
+                              isReverse: true,
+                              selectedBackgroundColor: greenColor,
+                              backgroundColor: purpleColor,
+                              transitionType: TransitionType.TOP_TO_BOTTOM,
+                              textStyle: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: whiteColor),
+                              onPress: () {},
+                            ),
                           ],
                         ),
                       ],
