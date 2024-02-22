a3e2af5b49428ed9c8d7545490fd7323dbc063a6
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-02-22 11:41:05 +0100

    ModifiedM lib/views/admin/clients_list.dart

--- lib/views/admin/clients_list.dart
+++ lib/views/admin/clients_list.dart
@@ -21,13 +21,14 @@ class _ClientsListState extends State<ClientsList> {
       "name": "Client ${index + 1}",
       "cin": List<String>.generate(8, (int index) => Random().nextInt(10).toString()).join(),
       "phone_number": "+216 ${Random().nextInt(90) + 10} ${Random().nextInt(900) + 100} ${Random().nextInt(900) + 100}",
-      "birth_date": formatDate(DateTime(Random().nextInt(20) + 1960, Random().nextInt(12) + 1, Random().nextInt(31) + 1), const <String>[]),
+      "birth_date": formatDate(DateTime(Random().nextInt(20) + 1960, Random().nextInt(12) + 1, Random().nextInt(31) + 1), const <String>[dd, ' / ', M, ' / ', yyyy]).toUpperCase(),
       "total_products": Random().nextInt(4000).toString(),
     },
   );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: scaffoldColor,
       body: Padding(
         padding: const EdgeInsets.all(24),
         child: Column(
@@ -50,40 +51,45 @@ class _ClientsListState extends State<ClientsList> {
             Container(width: MediaQuery.sizeOf(context).width, height: .3, color: greyColor, margin: const EdgeInsets.symmetric(vertical: 20)),
             Expanded(
               child: ListView.separated(
-                itemBuilder: (BuildContext context, int index) => Row(
-                  children: <Widget>[
-                    SizedBox(
-                      width: 250,
-                      height: 250,
-                      child: PrettyQrView.data(
-                        data: _clients[index].toString(),
-                        decoration: const PrettyQrDecoration(
-                          shape: PrettyQrSmoothSymbol(color: purpleColor),
-                          image: PrettyQrDecorationImage(image: AssetImage('assets/images/flutter.png'), fit: BoxFit.cover),
+                itemBuilder: (BuildContext context, int index) => Container(
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(15), color: darkColor),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      SizedBox(
+                        width: 250,
+                        height: 250,
+                        child: PrettyQrView.data(
+                          data: _clients[index].toString(),
+                          decoration: const PrettyQrDecoration(
+                            shape: PrettyQrSmoothSymbol(color: purpleColor),
+                            image: PrettyQrDecorationImage(image: AssetImage('assets/images/flutter.png'), fit: BoxFit.cover),
+                          ),
                         ),
                       ),
-                    ),
-                    const SizedBox(width: 20),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        for (final MapEntry<String, dynamic> entry in _clients[index].entries)
-                          Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: <Widget>[
-                              Container(
-                                padding: const EdgeInsets.all(4),
-                                decoration: BoxDecoration(color: purpleColor, borderRadius: BorderRadius.circular(5)),
-                                child: Text(entry.key.replaceAll("_", " ").toUpperCase(), style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: whiteColor)),
-                              ),
-                              const SizedBox(width: 10),
-                              Text(entry.value.toUpperCase(), style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: whiteColor)),
-                            ],
-                          ),
-                      ],
-                    ),
-                  ],
+                      const SizedBox(height: 20),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          for (final MapEntry<String, dynamic> entry in _clients[index].entries)
+                            Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                Container(
+                                  padding: const EdgeInsets.all(4),
+                                  decoration: BoxDecoration(color: purpleColor, borderRadius: BorderRadius.circular(5)),
+                                  child: Text(entry.key.replaceAll("_", " ").toUpperCase(), style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: whiteColor)),
+                                ),
+                                const SizedBox(width: 10),
+                                Text(entry.value.toUpperCase(), style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: whiteColor)),
+                              ],
+                            ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
                 separatorBuilder: (BuildContext context, int index) => const SizedBox(height: 20),
                 itemCount: _clients.length,
