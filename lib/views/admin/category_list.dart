c74524cf8f5df9c8fb697986641ee7ad818adab1
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-02-22 10:40:30 +0100

    ModifiedM lib/views/admin/category_list.dart

--- lib/views/admin/category_list.dart
+++ lib/views/admin/category_list.dart
@@ -2,7 +2,9 @@ import 'dart:math';
 
 import 'package:blacklist/utils/shared.dart';
 import 'package:blacklist/views/admin/products_table.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_animated_button/flutter_animated_button.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -89,7 +91,144 @@ class _CategoryListState extends State<CategoryList> {
                         transitionType: TransitionType.TOP_TO_BOTTOM,
                         textStyle: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: whiteColor),
                         onPress: () {
-                          showModalBottomSheet<void>(context: context, builder: (BuildContext context) => Container());
+                          showModalBottomSheet<void>(
+                            context: context,
+                            builder: (BuildContext context) => Container(
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: <Widget>[
+                                  AnimatedButton(
+                                    width: 80,
+                                    height: 30,
+                                    text: 'ADD',
+                                    selectedTextColor: whiteColor,
+                                    animatedOn: AnimatedOn.onHover,
+                                    animationDuration: 500.ms,
+                                    isReverse: true,
+                                    selectedBackgroundColor: darkColor,
+                                    backgroundColor: greenColor,
+                                    transitionType: TransitionType.TOP_TO_BOTTOM,
+                                    textStyle: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: whiteColor),
+                                    onPress: () {
+                                      final TextEditingController categoryName = TextEditingController();
+                                      final FocusNode categoryNode = FocusNode();
+                                      showModalBottomSheet<void>(
+                                        context: context,
+                                        builder: (BuildContext context) => Container(
+                                          padding: const EdgeInsets.all(16),
+                                          child: Column(
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            mainAxisSize: MainAxisSize.min,
+                                            children: <Widget>[
+                                              Text("Category Name", style: GoogleFonts.itim(fontSize: 18, fontWeight: FontWeight.w500, color: greyColor)),
+                                              const SizedBox(height: 10),
+                                              Container(
+                                                color: darkColor,
+                                                child: StatefulBuilder(
+                                                  builder: (BuildContext context, void Function(void Function()) _) {
+                                                    return TextField(
+                                                      onChanged: (String value) {
+                                                        if (value.trim().length <= 1) {
+                                                          _(() {});
+                                                        }
+                                                      },
+                                                      controller: categoryName,
+                                                      style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: greyColor),
+                                                      decoration: InputDecoration(
+                                                        contentPadding: const EdgeInsets.all(20),
+                                                        focusedBorder: const OutlineInputBorder(borderSide: BorderSide(color: purpleColor, width: 2, style: BorderStyle.solid)),
+                                                        border: InputBorder.none,
+                                                        hintText: "Choose a category name",
+                                                        hintStyle: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: greyColor),
+                                                        suffixIcon: categoryName.text.trim().isEmpty ? null : const Icon(FontAwesome.circle_check_solid, size: 15, color: greenColor),
+                                                      ),
+                                                      cursorColor: purpleColor,
+                                                    );
+                                                  },
+                                                ),
+                                              ),
+                                              const SizedBox(height: 20),
+                                              Row(
+                                                children: <Widget>[
+                                                  const Spacer(),
+                                                  AnimatedButton(
+                                                    width: 80,
+                                                    height: 30,
+                                                    text: 'ADD',
+                                                    selectedTextColor: whiteColor,
+                                                    animatedOn: AnimatedOn.onHover,
+                                                    animationDuration: 500.ms,
+                                                    isReverse: true,
+                                                    selectedBackgroundColor: darkColor,
+                                                    backgroundColor: greenColor,
+                                                    transitionType: TransitionType.TOP_TO_BOTTOM,
+                                                    textStyle: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: whiteColor),
+                                                    onPress: () {
+                                                      if (categoryName.text.trim().isNotEmpty) {
+                                                        showToast("New category has been added", greenColor);
+                                                        _categories.add(
+                                                          <String, dynamic>{
+                                                            "background_image": "assets/images/bg.jpg",
+                                                            "category_name": categoryName.text.trim(),
+                                                            "total_products": 0,
+                                                            "total_articles": 0,
+                                                            "state": false,
+                                                          },
+                                                        );
+                                                        Navigator.pop(context);
+                                                        setState(() {});
+                                                      } else {
+                                                        showToast("Please enter a category name", redColor);
+                                                      }
+                                                    },
+                                                  ),
+                                                  const SizedBox(width: 20),
+                                                  AnimatedButton(
+                                                    width: 80,
+                                                    height: 30,
+                                                    text: 'CANCEL',
+                                                    selectedTextColor: whiteColor,
+                                                    animatedOn: AnimatedOn.onHover,
+                                                    animationDuration: 500.ms,
+                                                    isReverse: true,
+                                                    selectedBackgroundColor: darkColor,
+                                                    backgroundColor: greyColor,
+                                                    transitionType: TransitionType.TOP_TO_BOTTOM,
+                                                    textStyle: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: whiteColor),
+                                                    onPress: () => Navigator.pop(context),
+                                                  ),
+                                                ],
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ).then(
+                                        (void value) {
+                                          categoryName.dispose();
+                                          categoryNode.dispose();
+                                        },
+                                      );
+                                    },
+                                  ),
+                                  const SizedBox(height: 20),
+                                  AnimatedButton(
+                                    width: 80,
+                                    height: 30,
+                                    text: 'DELETE',
+                                    selectedTextColor: whiteColor,
+                                    animatedOn: AnimatedOn.onHover,
+                                    animationDuration: 500.ms,
+                                    isReverse: true,
+                                    selectedBackgroundColor: darkColor,
+                                    backgroundColor: redColor,
+                                    transitionType: TransitionType.TOP_TO_BOTTOM,
+                                    textStyle: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: whiteColor),
+                                    onPress: () => setState(() => _deleteState = true),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
                         },
                       ),
                       const SizedBox(height: 10),
@@ -358,36 +497,51 @@ class _CategoryListState extends State<CategoryList> {
                                   ),
                                 ],
                               ),
-                              Row(
-                                mainAxisSize: MainAxisSize.min,
-                                children: <Widget>[
-                                  Row(
+                              GestureDetector(
+                                onTap: () => true,
+                                child: Container(
+                                  padding: const EdgeInsets.all(4),
+                                  decoration: BoxDecoration(color: scaffoldColor, borderRadius: BorderRadius.circular(5)),
+                                  child: Row(
                                     mainAxisSize: MainAxisSize.min,
                                     children: <Widget>[
-                                      const Icon(FontAwesome.gift_solid, size: 15, color: greenColor),
-                                      const SizedBox(width: 10),
-                                      StatefulBuilder(
-                                        builder: (BuildContext context, void Function(void Function()) _) {
-                                          return Checkbox(
-                                            activeColor: purpleColor,
-                                            value: item["gift_state"],
-                                            onChanged: (bool? value) => _(() => item["gift_state"] = value),
-                                          );
-                                        },
+                                      Row(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: <Widget>[
+                                          const Icon(FontAwesome.gift_solid, size: 15, color: greenColor),
+                                          const SizedBox(width: 10),
+                                          StatefulBuilder(
+                                            builder: (BuildContext context, void Function(void Function()) _) {
+                                              return Checkbox(
+                                                activeColor: purpleColor,
+                                                value: item["gift_state"],
+                                                onChanged: (bool? value) => _(() => item["gift_state"] = value),
+                                              );
+                                            },
+                                          ),
+                                        ],
                                       ),
+                                      const SizedBox(width: 10),
+                                      if (_deleteState)
+                                        Row(
+                                          mainAxisSize: MainAxisSize.min,
+                                          children: <Widget>[
+                                            const Icon(FontAwesome.circle_xmark_solid, size: 15, color: redColor),
+                                            const SizedBox(width: 10),
+                                            StatefulBuilder(
+                                              builder: (BuildContext context, void Function(void Function()) _) {
+                                                return Checkbox(
+                                                  activeColor: purpleColor,
+                                                  value: item["state"],
+                                                  onChanged: (bool? value) => _(() => item["state"] = value),
+                                                );
+                                              },
+                                            ),
+                                          ],
+                                        ),
                                     ],
                                   ),
-                                  if (_deleteState)
-                                    StatefulBuilder(
-                                      builder: (BuildContext context, void Function(void Function()) _) {
-                                        return Checkbox(
-                                          activeColor: purpleColor,
-                                          value: item["state"],
-                                          onChanged: (bool? value) => _(() => item["state"] = value),
-                                        );
-                                      },
-                                    ),
-                                ],
+                                ),
                               ),
                             ],
                           ),
