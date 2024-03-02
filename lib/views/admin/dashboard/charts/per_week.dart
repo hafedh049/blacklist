0127a31855710bc9af4220c9cde5062ad2982626
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-03-02 20:51:55 +0100

    ModifiedM lib/views/admin/dashboard/charts/per_week.dart

--- lib/views/admin/dashboard/charts/per_week.dart
+++ lib/views/admin/dashboard/charts/per_week.dart
@@ -17,7 +17,11 @@ class PerWeek extends StatefulWidget {
 
 class _PerWeekState extends State<PerWeek> {
   final List<String> _days = <String>["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
+
   final Map<int, double> _mappedData = <int, double>{for (int index = 1; index <= 7; index += 1) index: 0.0};
+
+  int touchedGroupIndex = -1;
+
   Future<bool> _load() async {
     final List<Map<String, dynamic>> data = (await FirebaseFirestore.instance
             .collection("sells")
@@ -59,77 +63,57 @@ class _PerWeekState extends State<PerWeek> {
             _mappedData[1] = 6;
             return _mappedData.values.every((double element) => element == 0.0)
                 ? Center(child: Text("NOT YET.", style: GoogleFonts.itim(fontSize: 22, fontWeight: FontWeight.w500, color: whiteColor)))
-                : BarChart(
-                    BarChartData(
-                      barTouchData: BarTouchData(
-                        touchTooltipData: BarTouchTooltipData(
-                          tooltipBgColor: Colors.grey,
-                          getTooltipItem: (a, b, c, d) => null,
-                        ),
-                        touchCallback: (FlTouchEvent event, response) {
-                          if (response == null || response.spot == null) {
-                            setState(() {
-                              touchedGroupIndex = -1;
-                              showingBarGroups = List.of(rawBarGroups);
-                            });
-                            return;
-                          }
-
-                          touchedGroupIndex = response.spot!.touchedBarGroupIndex;
-
-                          setState(() {
-                            if (!event.isInterestedForInteractions) {
-                              touchedGroupIndex = -1;
-                              showingBarGroups = List.of(rawBarGroups);
-                              return;
-                            }
-                            showingBarGroups = List.of(rawBarGroups);
-                            if (touchedGroupIndex != -1) {
-                              var sum = 0.0;
-                              for (final rod in showingBarGroups[touchedGroupIndex].barRods) {
-                                sum += rod.toY;
+                : StatefulBuilder(
+                    builder: (BuildContext context, void Function(void Function()) _) {
+                      return BarChart(
+                        BarChartData(
+                          barTouchData: BarTouchData(
+                            touchTooltipData: BarTouchTooltipData(tooltipBgColor: whiteColor, getTooltipItem: (BarChartGroupData a, int b, BarChartRodData c, int d) => null),
+                            touchCallback: (FlTouchEvent event, response) {
+                              if (response == null || response.spot == null) {
+                                _(() {
+                                  touchedGroupIndex = -1;
+                                });
+                                return;
                               }
-                              final avg = sum / showingBarGroups[touchedGroupIndex].barRods.length;
 
-                              showingBarGroups[touchedGroupIndex] = showingBarGroups[touchedGroupIndex].copyWith(
-                                barRods: showingBarGroups[touchedGroupIndex].barRods.map((rod) {
-                                  return rod.copyWith(toY: avg, color: widget.avgColor);
-                                }).toList(),
-                              );
-                            }
-                          });
-                        },
-                      ),
-                      titlesData: FlTitlesData(
-                        show: true,
-                        rightTitles: const AxisTitles(
-                          sideTitles: SideTitles(showTitles: false),
-                        ),
-                        topTitles: const AxisTitles(
-                          sideTitles: SideTitles(showTitles: false),
-                        ),
-                        bottomTitles: AxisTitles(
-                          sideTitles: SideTitles(
-                            showTitles: true,
-                            getTitlesWidget: bottomTitles,
-                            reservedSize: 42,
+                              touchedGroupIndex = response.spot!.touchedBarGroupIndex;
+
+                              _(() {
+                                if (!event.isInterestedForInteractions) {
+                                  touchedGroupIndex = -1;
+                                  return;
+                                }
+                                // showingBarGroups = List.of(rawBarGroups);
+                                if (touchedGroupIndex != -1) {
+                                  /*     var sum = 0.0;
+                                for (final rod in showingBarGroups[touchedGroupIndex].barRods) {
+                                  sum += rod.toY;
+                                }
+                                final avg = sum / showingBarGroups[touchedGroupIndex].barRods.length;
+
+                                showingBarGroups[touchedGroupIndex] = showingBarGroups[touchedGroupIndex].copyWith(
+                                  barRods: showingBarGroups[touchedGroupIndex].barRods.map((rod) {
+                                    return rod.copyWith(toY: avg, color: widget.avgColor);
+                                  }).toList(),
+                                );*/
+                                }
+                              });
+                            },
                           ),
-                        ),
-                        leftTitles: AxisTitles(
-                          sideTitles: SideTitles(
-                            showTitles: true,
-                            reservedSize: 28,
-                            interval: 1,
-                            getTitlesWidget: leftTitles,
+                          titlesData: FlTitlesData(
+                            show: true,
+                            rightTitles: const AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                            topTitles: const AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                            bottomTitles: AxisTitles(sideTitles: SideTitles(showTitles: true, getTitlesWidget: bottomTitles, reservedSize: 42)),
+                            leftTitles: AxisTitles(sideTitles: SideTitles(showTitles: true, reservedSize: 28, interval: 1, getTitlesWidget: leftTitles)),
                           ),
+                          borderData: FlBorderData(show: false),
+                          barGroups: showingBarGroups,
+                          gridData: const FlGridData(show: false),
                         ),
-                      ),
-                      borderData: FlBorderData(
-                        show: false,
-                      ),
-                      barGroups: showingBarGroups,
-                      gridData: const FlGridData(show: false),
-                    ),
+                      );
+                    },
                   );
           } else if (snapshot.connectionState == ConnectionState.waiting) {
             return const Loading();
