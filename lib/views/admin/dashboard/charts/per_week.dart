1774bd07f47ecfe0abb901089e3cd6b3a8b1df19
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-03-02 20:41:55 +0100

    ModifiedM lib/views/admin/dashboard/charts/per_week.dart  M pubspec.yaml

--- lib/views/admin/dashboard/charts/per_week.dart
+++ lib/views/admin/dashboard/charts/per_week.dart
@@ -1,10 +1,10 @@
 import 'package:blacklist/utils/helpers/errored.dart';
 import 'package:blacklist/utils/helpers/loading.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:syncfusion_flutter_charts/sparkcharts.dart';
 
 import '../../../../utils/shared.dart';
 
@@ -19,7 +19,7 @@ class _PerWeekState extends State<PerWeek> {
   final List<String> _days = <String>["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
   final Map<int, double> _mappedData = <int, double>{for (int index = 1; index <= 7; index += 1) index: 0.0};
   Future<bool> _load() async {
-    /* final List<Map<String, dynamic>> data = (await FirebaseFirestore.instance
+    final List<Map<String, dynamic>> data = (await FirebaseFirestore.instance
             .collection("sells")
             .where(
               "timestamp",
@@ -41,7 +41,7 @@ class _PerWeekState extends State<PerWeek> {
           _mappedData[index] = _mappedData[index]! + entry["quantity"] * entry["price"];
         }
       }
-    }*/
+    }
     return true;
   }
 
@@ -58,16 +58,78 @@ class _PerWeekState extends State<PerWeek> {
           if (snapshot.hasData) {
             _mappedData[1] = 6;
             return _mappedData.values.every((double element) => element == 0.0)
-                ? Center(
-                    child: Text(
-                      "NOT YET.",
-                      style: GoogleFonts.itim(fontSize: 22, fontWeight: FontWeight.w500, color: whiteColor),
+                ? Center(child: Text("NOT YET.", style: GoogleFonts.itim(fontSize: 22, fontWeight: FontWeight.w500, color: whiteColor)))
+                : BarChart(
+                    BarChartData(
+                      barTouchData: BarTouchData(
+                        touchTooltipData: BarTouchTooltipData(
+                          tooltipBgColor: Colors.grey,
+                          getTooltipItem: (a, b, c, d) => null,
+                        ),
+                        touchCallback: (FlTouchEvent event, response) {
+                          if (response == null || response.spot == null) {
+                            setState(() {
+                              touchedGroupIndex = -1;
+                              showingBarGroups = List.of(rawBarGroups);
+                            });
+                            return;
+                          }
+
+                          touchedGroupIndex = response.spot!.touchedBarGroupIndex;
+
+                          setState(() {
+                            if (!event.isInterestedForInteractions) {
+                              touchedGroupIndex = -1;
+                              showingBarGroups = List.of(rawBarGroups);
+                              return;
+                            }
+                            showingBarGroups = List.of(rawBarGroups);
+                            if (touchedGroupIndex != -1) {
+                              var sum = 0.0;
+                              for (final rod in showingBarGroups[touchedGroupIndex].barRods) {
+                                sum += rod.toY;
+                              }
+                              final avg = sum / showingBarGroups[touchedGroupIndex].barRods.length;
+
+                              showingBarGroups[touchedGroupIndex] = showingBarGroups[touchedGroupIndex].copyWith(
+                                barRods: showingBarGroups[touchedGroupIndex].barRods.map((rod) {
+                                  return rod.copyWith(toY: avg, color: widget.avgColor);
+                                }).toList(),
+                              );
+                            }
+                          });
+                        },
+                      ),
+                      titlesData: FlTitlesData(
+                        show: true,
+                        rightTitles: const AxisTitles(
+                          sideTitles: SideTitles(showTitles: false),
+                        ),
+                        topTitles: const AxisTitles(
+                          sideTitles: SideTitles(showTitles: false),
+                        ),
+                        bottomTitles: AxisTitles(
+                          sideTitles: SideTitles(
+                            showTitles: true,
+                            getTitlesWidget: bottomTitles,
+                            reservedSize: 42,
+                          ),
+                        ),
+                        leftTitles: AxisTitles(
+                          sideTitles: SideTitles(
+                            showTitles: true,
+                            reservedSize: 28,
+                            interval: 1,
+                            getTitlesWidget: leftTitles,
+                          ),
+                        ),
+                      ),
+                      borderData: FlBorderData(
+                        show: false,
+                      ),
+                      barGroups: showingBarGroups,
+                      gridData: const FlGridData(show: false),
                     ),
-                  )
-                : SfSparkBarChart(
-                    plotBand: SparkChartPlotBand(),
-                    labelDisplayMode: SparkChartLabelDisplayMode.all,
-                    data: _mappedData.values.toList(),
                   );
           } else if (snapshot.connectionState == ConnectionState.waiting) {
             return const Loading();
