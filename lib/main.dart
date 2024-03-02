a851f6ae93e8256dc20da1ecc7992611a1ea5217
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-03-02 13:44:20 +0100

    Modified  lib/main.dart A  lib/utils/helpers/errored.dart M  lib/utils/helpers/loading.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 
+import 'utils/helpers/errored.dart';
 import 'utils/helpers/loading.dart';
 
 void main() {
--- lib/utils/helpers/loading.dart
+++ lib/utils/helpers/loading.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
 
-class Loading extends StatefulWidget {
-  const Loading({super.key});
+import '../shared.dart';
 
-  @override
-  State<Loading> createState() => _LoadingState();
-}
+class Errored extends StatelessWidget {
+  final String error;
+  const Errored({super.key, required this.error});
 
-class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: LottieBuilder.asset("assets/lotties/loading.json")));
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            LottieBuilder.asset("assets/lotties/error.json"),
+            Text(error, style: GoogleFonts.itim(fontSize: 16, fontWeight: FontWeight.w500, color: greyColor)),
+          ],
+        ),
+      ),
+    );
   }
 }
